31af6bbcbe18e3b0614335d419e8df78389a50fb
NewPing <jan-roth1998@web.de>
2022-07-16 14:12:24 +0200

    init workspace

---
+++ tbPlay.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class tbPlay : TextureButton
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	public void _on_TextureButton_pressed()
+	{
+		GetTree().ChangeScene("res://Game.tscn");
+	}
+
+}
