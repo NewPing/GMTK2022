1c8cf8626aed039f6e1ef66f491ee4075d0d77a8
NewPing <jan-roth1998@web.de>
2022-07-16 21:17:35 +0200

    .

---
+++ Knight2.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public class Knight2 : KinematicBody2D
+{
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+	
+	
+}
